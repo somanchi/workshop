1fbd469132f11fb306fbbe8ca3345fbd20681e6d
root <root@11CPU0223L.amritavidya.edu>
2016-06-02 09:23:37 +0530

    third commit

--- prasad/urls.py
+++ prasad/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
-admin.autodiscover()
-
+from registration.views import Home
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'prasad.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', Home.as_view(), name='home')
 )
