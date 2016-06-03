6e17aae1340713a3e3ba597e68685d294df1f119
root <root@11CPU0223L.amritavidya.edu>
2016-06-03 09:55:40 +0530

    fourth commit

--- registration/templates/models.py
+++ registration/templates/models.py
@@ -1,11 +1,8 @@
 from django.db import models
-
-# Create your models here.
-from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.utils.translation import ugettext_lazy as _
 from django.core.validators import RegexValidator, MaxValueValidator, MinValueValidator
-
+from mail_templated import send_mail
 
 # Create your models here.
 GENDER = (('M', _('Male')), ('F', _('Female')))
--- registration/urls.py
+++ registration/urls.py
@@ -2,14 +2,12 @@ from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
 from registration.views import Home
-from registration import urls as register
-
+from registration.views import UserRegistrationView
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'prasad.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^register/', include(register)),
-    url(r'^$', Home.as_view(), name='home')
+   
+    url(r'^registration/', UserRegistrationView.as_view(), name='register_user')
 )
